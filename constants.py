c5c9ab3db738712df58f98c604628b19286aa4a8
Quentin <Coulis1990@gmail.com>
2019-02-04 16:29:31 +0100

    Separated files

---
+++ constants.py
@@ -0,0 +1,17 @@
+TERMINAL_SIZE_X = 30
+TERMINAL_SIZE_Y = 19
+
+MESSAGE_PANEL_X = 0
+MESSAGE_PANEL_W = TERMINAL_SIZE_X
+MESSAGE_PANEL_H = 5
+MESSAGE_PANEL_Y = TERMINAL_SIZE_Y - MESSAGE_PANEL_H
+
+QUIT_KEY = (41, 224)
+ACTION_KEY = (44,)
+MOVEMENT_KEYS = {93: [0, 0], 90: [0, 1], 89: [-1, 1], 92: [-1, 0], 95: [-1, -1], 96: [0, -1], 97: [1, -1],
+                 94: [1, 0], 91: [1, 1]}
+
+TEAM_COLORS = ['blue', 'red', 'green', 'yellow']
+ACTIVE_COLOR = 'green'
+
+KEY = {'tab': 43}
