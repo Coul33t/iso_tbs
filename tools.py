ab6bf8822256bfe9c57f7f1c5151b6a970ef6bba
Quentin <Coulis1990@gmail.com>
2019-02-21 16:34:34 +0100

    New Rect class (mainly for the UI)

--- tools.py
+++ tools.py
@@ -9,6 +9,31 @@ class Point:
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
+# Do I really need to put some docstring for this?
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        # Because this will be easier to use
+        # Top left corner
+        self.top_left = Point(x, y)
+        # Top right
+        self.top_right = Point(x+w, y)
+        # Bottom right corner
+        self.bottom_right = Point(x+w, y+h)
+        # Bottom left corner
+        self.bottom_left = Point(x, y+h)
+
+        self.top = y
+        self.left = x
+        self.bottom = y+h
+        self.right = x+w
+
+    def inside(self, pt):
+        return pt.x > self.x and pt.x < self.x + self.w and pt.y > self.y and pt.y < self.y + self.h
+
 # Euclidean distance
 def dst_euc(p1, p2=Point(0,0)):
     return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2))
@@ -23,3 +48,4 @@ def point_in(ptn, lst):
             return True
 
     return False
+
